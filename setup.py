8a94714ba3714cffb52ce92486871f5de400cfbd
slavo-heys <ilodz24hd@gmail.com>
2024-10-29 19:36:16 +0100

    #5

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ data rozpoczęcia: 27.10.2024
 
 from tkinter import *
 from tkinter import messagebox
+import random
+from datetime import datetime
+import time
 
 class Program:
     def __init__(self) -> None:        
@@ -20,6 +23,10 @@ class Program:
         self.wiek_inp = StringVar()
         self.podstawa_inp = StringVar()
         self.ilosc_inp = StringVar()
+        self.wynik = StringVar()
+        self.i = 0
+        self.poprawne = 0
+        self.bledna = 0
                 
         # menu na listwie górnej
         menu = Menu(self.root)
@@ -101,6 +108,32 @@ class Program:
         self.input_podstawa_dodawania.delete(0, END)
         self.input_ilosc_dzialan.delete(0, END)
 
+    def poprawny_wynik(self,wynik_user):
+        wynik_p = self.wynik.get()
+        wynik_p = int(wynik_p)
+        wynik_user_p = wynik_user
+        wynik_user_p = int(wynik_user_p)
+
+        
+
+        if wynik_user_p == wynik_p:
+            self.ramka3 = LabelFrame(self.root, text="Sprawdzanie poprawności:", fg = "green", padx = 2, pady = 5)
+            self.ramka3.pack(padx=0, pady=5) 
+            print("wynik poprawny")
+            text6 = Label(self.ramka3, text="Wynik poprawny!!!", font=("Arial", 12), fg= "green")
+            text6.pack(padx=0, pady=5)
+            time.sleep(3)
+        else:
+            self.ramka3 = LabelFrame(self.root, text="Sprawdzanie poprawności:", fg = "green", padx = 2, pady = 5)
+            self.ramka3.pack(padx=0, pady=5) 
+            print("wynik niepoprawny")
+            text6 = Label(self.ramka3, text="Wynik niepoprawny!!!", font=("Arial", 12), fg= "red")
+            text6.pack(padx=0, pady=5)
+            time.sleep(3)        
+        
+        self.ramka3.destroy()
+        
+
     def dzialanie_dodawanie(self):
         self.imie = self.imie_inp.get()
         self.wiek = self.wiek_inp.get()
@@ -128,10 +161,39 @@ class Program:
             self.podstawa = 50
             self.ilosc = 100   
 
-        print(self.imie)
-        
-        
+        self.ramka1.destroy()
 
+        self.ramka2 = LabelFrame(self.root, text="Działanie: ", fg = "green", padx = 2, pady = 5)
+        self.ramka2.pack(padx=0, pady=20) 
+
+        text1 = Label(self.ramka2, text = "Ile jest:", font=("Arial", 12, "bold"), foreground="silver")
+        text1.grid(row=0, column=1)
+
+        for self.i in range(self.ilosc):
+            a = random.randint(1, int(self.podstawa))
+            b = random.randint(1, int(self.podstawa))           
+
+            suma_wynik = a + b
+            
+            text2 = Label(self.ramka2)
+            text2.grid(row=1, column=0)
+
+            text3 = Label(self.ramka2, text = str(a)+" + "+str(b)+" = ", font=("Arial", 20), foreground="green")
+            text3.grid(row=2, column=1)
+            
+            text4 = Entry(self.ramka2, textvariable=self.wynik, width = 5, font=("Arial", 20), fg= "white", bg = "green")
+            text4.grid(row=2, column=2)
+
+            text5 = Label(self.ramka2)
+            text5.grid(row=3, column=0)
+
+            button = Button(self.ramka2, text="Sprawdź wynik", fg="white", bg="red", command=lambda: self.poprawny_wynik(suma_wynik))
+            button.grid(row=4,column=1)
+
+            button1 = Button(self.ramka2, text="Popraw wynik", fg="white", bg="black", command=text4.delete(0,END))
+            button1.grid(row=4,column=2)
+
+            
     def kliknij_wczytaj(self, event=None):
         print("Kliknąłeś otwórz")
 
--- test3.py
+++ test3.py
@@ -1,24 +1,16 @@
 import tkinter as tk
 
-def new_file_clicked(event=None):
-    print("The New File menu was clicked!")
+def function(widget):
+    print(widget['text'])
 
 root = tk.Tk()
-root.title("Menubar in Tk")
-root.geometry("400x300")
-menubar = tk.Menu()
-file_menu = tk.Menu(menubar, tearoff=False)
-new_file_img = tk.PhotoImage(file="new_file.png")
-file_menu.add_command(
-    label="New",
-    accelerator="Ctrl+N",
-    command=new_file_clicked,
-    image=new_file_img,
-    # The image must be placed to the left of the text.
-    compound=tk.LEFT
-)
-root.bind_all("<Control-n>", new_file_clicked)
-root.bind_all("<Control-N>", new_file_clicked)
-menubar.add_cascade(menu=file_menu, label="File")
-root.config(menu=menubar)
+
+button1 = tk.Button(root, text='Yes')
+button1.pack()
+button1["command"] = lambda:function(button1)
+
+button2 = tk.Button(root, text='No')
+button2.pack()
+button2["command"] = lambda:function(button2)
+
 root.mainloop()
